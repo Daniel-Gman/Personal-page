6cd469778649e5350f840f72cc1b91b79f35a36f
Daniel Vermeulen <34882@ma-web.nl>
2023-07-06 16:21:50 +0200

    back-end

---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php
+$dbname = "formulier.db";
+
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+try {
+    $conn = new PDO("sqlite:$dbname");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $insertQuery = "INSERT INTO users (email, username, password) VALUES (:email, :username, :password)";
+    $stmt = $conn->prepare($insertQuery);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+
+    header("location: login.html");
+    exit;
+} catch (PDOException $e) {
+    die("Error connecting to the database: " . $e->getMessage());
+}
+?>
---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+$dbname = "formulier.db";
+
+$login_username = $_POST['username'];
+$login_password = $_POST['password'];
+
+try {
+    $conn = new PDO("sqlite:$dbname");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $selectQuery = "SELECT * FROM users WHERE username = :username";
+    $stmt = $conn->prepare($selectQuery);
+    $stmt->bindParam(':username', $login_username);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && $user['password'] === $login_password) {
+        header("location: Index.html");
+        exit;
+    } else {
+        echo "Login doesn't match!";
+    }
+} catch (PDOException $e) {
+    die("Error connecting to the database: " . $e->getMessage());
+}
+?>
